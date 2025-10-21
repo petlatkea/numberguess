5fa1f4519f576d84d404cf07d91780d0ded0a257
Peter Lind (@EK) <petlatkea@peterlind.dk>
2025-10-21 19:37:14 +0200

    added number - and comparisons with guess

--- script.js
+++ script.js
@@ -2,6 +2,8 @@
 
 window.addEventListener("DOMContentLoaded", main);
 
+const number = 42;
+
 function main() {
     console.log("JavaScript kører!");
 
@@ -15,4 +17,19 @@ function buttonClicked() {
     // NOTE: bruger https://developer.mozilla.org/en-US/docs/Web/API/HTMLInputElement/valueAsNumber
     const guess = document.querySelector("#guess").valueAsNumber;
     console.log(guess);
+
+    // sammenlign gæt med tal
+    // hvis det er for højt - skriv "Du gættede for højt"
+    if (guess > number) {
+        console.log("Du gættede for højt");
+    } 
+    // hvis det var for lavt - skriv "Du gættede for lavt"
+    if (guess < number) {
+        console.log("Du gættede for lavt");
+    }
+    // hvis det var korrekt - skriv "Det var korrekt" - og stop
+    if (guess == number) {
+        console.log("Du gættede korrekt");
+    }
+
 }
