08eb964bab3486630d47e2c28916a5a0fefaa5ad
Peter Lind (@EK) <petlatkea@peterlind.dk>
2025-10-21 19:28:14 +0200

    added eventlistener for the button

--- script.js
+++ script.js
@@ -4,4 +4,10 @@ window.addEventListener("DOMContentLoaded", main);
 
 function main() {
     console.log("JavaScript kører!");
+
+    document.querySelector("#btn_guess").addEventListener("click", buttonClicked);
+}
+
+function buttonClicked() {
+    console.log("Der blev klikket på knappen");
 }
